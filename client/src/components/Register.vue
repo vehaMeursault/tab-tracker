db769fc6eab3618d910543c6c94c6464fb6ff1db
Victor Kyrg <victorkyrg@gmail.com>
2021-03-29 15:50:06 +0200

    quick save

---
+++ client/src/components/Register.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Register</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped></style>
--- server/src/app.js
+++ server/src/app.js
@@ -8,14 +8,20 @@ app.use(express.json()); // we can handle JSON data
 app.use(express.urlencoded({extended: false})); // we can handle URL encoded data
 app.use(cors());
 
-app.use((req, res) => {
+app.use((req, res, next) => {
     console.log('MW triggered');
-    res.sendStatus(200);
+    // res.sendStatus(200);
+    next();
 });
 
 app.get('*', (req, res) => {
-    console.log('test');
-    res.send('test');
+    res.send('GET request received');
+})
+
+app.post('/register', (req, res) => {
+    res.send({
+        message: `User registered: ${req.body.email}.`
+    })
 })
 
 const port = process.env.PORT || 8081;
